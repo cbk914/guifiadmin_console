a4f9cc41ad0652e34de14195eed93b02c027ca49
cbk914 <cbk914@riseup.net>
2015-10-22 02:35:16 +0200

    Update guifiadmin_console.py

--- guifiadmin_console.py
+++ guifiadmin_console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #-*-coding: utf-8-*-
 # Consola d'Admin de Guifi.net
-# Version 0.1-61 alpha - 21/10/2015
+# Version 0.1-62 alpha - 21/10/2015
 # Coded by cbk
 # Copyleft 2015
 # TO-DO:
@@ -169,30 +169,6 @@ def main():
 		cmd1 = os.system("mtr "+IP)
 		main()
 
-# Escanear con Lynis
-	if opcion == "es":
-			if os.path.exists("/usr/sbin/lynis"):
-				instalado = 1
-			else:
-				print "Lynis no esta instalado en la ruta por defecto."
-				instalar=raw_input("Pulsa (I) para instalar Lynis, cualquier otra tecla para volver al menú principal.")
-				if instalar == "I" or instalar == "i":
-					print "Instalando Lynis..."
-					cmd1 = os.system("aptitude update && aptitude -y install lynis")
-					raw_input("Pulsa una tecla para continuar...")
-					main()
-				else:
-					main()
-	clearScreen()	
-	cmd1 = os.system("sudo /usr/sbin/lynis audit system")
-	an1=raw_input("Analizar Manualmente los Resultados? S/N ")
-	if an1 == "S":
-		cmd2=os.system("sudo vim /var/log/lynis.log")
-	else:
-		if an1 == "N":
-			print "Saliendo al Menu Principal"
-			main()
-				
 # Escaneo profundo de red
 	if opcion == "nm":
 		clearScreen()
@@ -237,8 +213,55 @@ def main():
 		raw_input("Pulsa una tecla para continuar...")
 		main()
 
+# Escanear con Lynis
+        if opcion == "es":
+                        if os.path.exists("/usr/sbin/lynis"):
+                                instalado = 1
+                        else:
+                                print "Lynis no esta instalado en la ruta por defecto."
+                                instalar=raw_input("Pulsa (I) para instalar Lynis, cualquier otra tecla para volver al menú principal.")
+                                if instalar == "I" or instalar == "i":
+                                        print "Instalando Lynis..."
+                                        cmd1 = os.system("aptitude update && aptitude -y install lynis")
+                                        raw_input("Pulsa una tecla para continuar...")
+                                        main()
+                                else:
+                                        main()
+        clearScreen()
+        cmd1 = os.system("sudo /usr/sbin/lynis audit system")
+        an1=raw_input("Analizar Manualmente los Resultados? S/N ")
+        if an1 == "S":
+                cmd2=os.system("sudo vim /var/log/lynis.log")
+        else:
+                if an1 == "N":
+                        print "Saliendo al Menu Principal"
+                        main()
+
+
 
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
